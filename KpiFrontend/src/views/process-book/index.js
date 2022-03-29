8d9fb68e589926004c73a412952f0c8456f44193
Anna Dobroszek <Anna.Dobroszek@rossmann.com.pl>
2022-03-29 11:42:50 +0200

    #added table dimensions

--- KpiFrontend/src/views/process-book/index.js
+++ KpiFrontend/src/views/process-book/index.js
@@ -2,26 +2,76 @@ import { useEffect, useState } from 'react';
 
 // material-ui
 import { Typography } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
 import { loadDetails } from '../../services/processBookService';
 
+
+
 // ==============================|| SAMPLE PAGE ||============================== //
 
 const ProcessBook = () => {
-    const [data, setData] = useState();
-    useEffect(() => {
+    const [data, setData] = useState([]);
+    //const [row, setRow] = useState();
+    //var rows=[];
+
+
+    const getData = async => {
         loadDetails().then((x) => {
-            setData(x);
             console.log(x[0].dimensionDescription);
+            setData(x);
         });
+    }
+
+    useEffect(() => {
+        getData();
     }, []);
+
+    const columns = [
+    { field: 'idDimension', headerName: 'IdDimension' },
+    {
+        field: 'dimensionName',
+        headerName: 'DimensionName',
+        width: 150,
+    },
+    {
+        field: 'dimensionName',
+        headerName: 'DimensionName',
+        width: 250,
+    },
+    {
+        field: 'dimensionDescription',
+        headerName: 'DimensionDescription',
+        width: 210,
+    },
+    {
+        field: 'overDimensionsId',
+        headerName: 'OverDimensionsId', 
+        width: 210    
+    }
+    ];
+
+    const rows = data.map((row) => ({
+        idDimension: row.idDimension,
+        dimensionName: row.dimensionName,
+        dimensionDescription: row.dimensionDescription,
+        overDimensionsId: row.overDimensionsId
+    }));
+    
     return (
         <MainCard title="Słownik zaciągnięty z bazy">
-            <Typography variant="body2">
-                Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut
-            </Typography>
+            <div style={{height: 500, width:"100%"}}>
+           <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            checkboxSelection
+            disableSelectionOnClick
+            getRowId={(row) => row.idDimension}
+      /></div>
         </MainCard>
     );
 };
