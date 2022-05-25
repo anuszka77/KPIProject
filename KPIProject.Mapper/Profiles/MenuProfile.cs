cc9e688aecb2131fe1fc2ce6cad03710b3af8b46
Anna Dobroszek <Anna.Dobroszek@rossmann.com.pl>
2022-05-25 22:55:48 +0200

    #fixed after merge

--- KPIProject.Mapper/Profiles/MenuProfile.cs
+++ KPIProject.Mapper/Profiles/MenuProfile.cs
@@ -35,10 +35,10 @@ namespace KPIProject.Mapper.Profiles
             CreateMap<FGetListOfSystemDictionary_Result, FGetListOfSystemDictionary_ResultDTO>().ReverseMap();
             //Proste słowniki_end
             CreateMap<FGetLayersBySysDimTier_Result, FGetLayersBySysDimTier_ResultDTO>().ReverseMap();
-        }
-            CreateMap<FGetListOfKpiDictionary_Result, FGetListOfKpiDictionary_ResultDTO>().ReverseMap();
-            CreateMap<FGetListOfCriticalToDictionary_Result, FGetListOfCriticalToDictionary_ResultDTO>().ReverseMap();
+
             CreateMap<TiersDictionary, TiersDictionaryDTO>().ReverseMap();
+        }
+           
 
 
 
